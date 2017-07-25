5889a47104a91d10847f82e46236dbd7e1438115
suthagar23 <suthagar.14@cse.mrt.ac.lk>
2017-07-25 21:54:24 +0530

    Encounters route management

--- app/js/main/route-management.js
+++ app/js/main/route-management.js
@@ -6,7 +6,27 @@ var metadataadminModule =angular.module('metadataadminModule',
 metadataadminModule.config(['$routeProvider',
                     function ($routeProvider) {
                       $routeProvider.
- 
+//                        when('/manage-encounters/:encounterUUID', {
+//                          templateUrl: 'js/encounters/manage-encounters.html',
+//                          controller: 'manageEncounterCtrl',
+//                        }).
+                        when('/add-encounter', {
+                          templateUrl: 'js/encounters/manage-encounters.html',
+                          controller: 'AddNewEncounterCtrl',
+                        }).
+                        when('/manage-encounter-types', {
+                          templateUrl: 'js/encounters/manage-encounter-type.html',
+                          controller: 'manageEncounterTypeCtrl',
+                        }).
+                        when('/manage-encounter-roles', {
+                          templateUrl: 'js/encounters/manage-encounter-role.html',
+                          controller: 'manageEncounterRoleCtrl',
+                        }).
+                        otherwise({
+                          templateUrl: 'js/home/home.html',
+                          controller: 'mainHomeCtrl',
+                        
+                        });
                     }]);
 
 
